1c34f6837fda6215c43ebfcc264963728ad21eb9
Andres Hazard <andres.hazard@servicerocket.com>
2016-10-14 02:50:20 -0300

    #6 add tests for dao saverequest method

---
+++ src/test/groovy/StringTool/StringToolSpec.groovy
@@ -0,0 +1,33 @@
+package StringTool
+
+import spock.lang.Specification
+/**
+ * Created by andreshazard on 10/14/16.
+ */
+class StringToolSpec extends Specification {
+
+
+    def "a non digit string should return false when calling isNumber"() {
+
+        given: "a non digit string"
+        String nonDigit = "letters"
+
+        when: "isNumber is called"
+        boolean answer = StringTool.isNumber(nonDigit)
+
+        then: "it will return false"
+        assert (answer == false)
+    }
+
+    def "a digit string should return true when calling isNumber"() {
+
+        given: "a digit string"
+        String nonDigit = "5"
+
+        when: "isNumber is called"
+        boolean answer = StringTool.isNumber(nonDigit)
+
+        then: "it will return true"
+        assert (answer == true)
+    }
+}
--- src/test/groovy/telegram/TelegramBotSpec.groovy
+++ src/test/groovy/telegram/TelegramBotSpec.groovy
@@ -4,8 +4,10 @@ import com.pokebotgo.Dao
 import org.telegram.telegrambots.api.methods.send.SendMessage
 import org.telegram.telegrambots.api.objects.Message
 import org.telegram.telegrambots.api.objects.Update
+import pokemon.Pokemon
 import spock.lang.Specification
 import telegram.TelegramBot
+import type.Type
 
 class TelegramBotSpec extends Specification {
 
@@ -22,7 +24,7 @@ class TelegramBotSpec extends Specification {
     SendMessage sendMessageRequest = Mock();
 
     @Collaborator
-    Dao dao = Stub();
+    Dao dao = Mock();
 
     def "when update has no message nothing should be return to the user"() {
 
@@ -34,6 +36,18 @@ class TelegramBotSpec extends Specification {
 
         then: "no message should be return to the user"
         0 * sendMessageRequest.setText(_ as String)
+    }
+
+    def "when update has no message nothing should log into the db"() {
+
+        given: "an update request with no message"
+        update.hasMessage() >> false
+
+        when: "onUpdateReceived is called"
+        telegramBot.onUpdateReceived(update)
+
+        then: "it will not be log the the db"
+        0 * dao.saveBotUsage(_ as String)
     }
 
     def "when update has message '/start' then a greeting should be send to the user"() {
@@ -56,6 +70,25 @@ class TelegramBotSpec extends Specification {
 
     }
 
+    def "when update has message '/start' it will be log into the database"() {
+
+        given: "an update request with message '/start' "
+        update.hasMessage() >> true
+        update.getMessage() >> message
+        message.hasText() >> true
+        message.getText() >> "/start"
+
+        when: "onUpdateReceived is called"
+        telegramBot.onUpdateReceived(update)
+
+        then: "it will be log the the db"
+        1 * dao.saveBotUsage("/start")
+
+        and: "expect error since we are not sending the message when testing"
+        thrown(NullPointerException)
+
+    }
+
     def "when update has a message that is not a command a default message should be sent"(){
 
         given: "an update request with a message that is not a command"
@@ -70,6 +103,25 @@ class TelegramBotSpec extends Specification {
         then: "a message will ask the user to use a command"
         1 * sendMessageRequest.setText('Please use one of the commands, followed by a valid parameter\nEg: /pokemon pikachu' +
                 '\nEg: /type fire\nEg: /pokedex 1')
+
+        and: "expect error since we are not sending the message when testing"
+        thrown(NullPointerException)
+
+    }
+
+    def "when update has a message that is not a command nothing will be log to the db"(){
+
+        given: "an update request with a message that is not a command"
+        update.hasMessage() >> true
+        update.getMessage() >> message
+        message.hasText() >> true
+        message.getText() >> "/not_a_valid_command"
+
+        when: "onUpdateReceived is called"
+        telegramBot.onUpdateReceived(update)
+
+        then: "it will not be log the the db"
+        0 * dao.saveBotUsage(_ as String)
 
         and: "expect error since we are not sending the message when testing"
         thrown(NullPointerException)
@@ -97,6 +149,26 @@ class TelegramBotSpec extends Specification {
 
     }
 
+    def "when update has a valid command /pokemon but no parameter it will be log the db"() {
+
+        given: "an update request with a valid command but no parameter"
+        update.hasMessage() >> true
+        update.getMessage() >> message
+        message.hasText() >> true
+        message.getText() >> "/pokemon"
+
+        when: "onUpdateReceived is called"
+        telegramBot.onUpdateReceived(update)
+
+        then: "it will be log the the db"
+        1 * dao.saveBotUsage('/pokemon')
+
+        and: "expect error since we are not sending the message when testing"
+        thrown(NullPointerException)
+
+
+    }
+
     def "when update has a valid command /type but no parameter a default message should be sent"() {
 
         given: "an update request with a valid command but no parameter"
@@ -114,8 +186,24 @@ class TelegramBotSpec extends Specification {
 
         and: "expect error since we are not sending the message when testing"
         thrown(NullPointerException)
+    }
 
+    def "when update has a valid command /type but no parameter it will be log to the db"() {
 
+        given: "an update request with a valid command but no parameter"
+        update.hasMessage() >> true
+        update.getMessage() >> message
+        message.hasText() >> true
+        message.getText() >> "/type"
+
+        when: "onUpdateReceived is called"
+        telegramBot.onUpdateReceived(update)
+
+        then: "it will be log the the db"
+        1 * dao.saveBotUsage('/type')
+
+        and: "expect error since we are not sending the message when testing"
+        thrown(NullPointerException)
     }
 
     def "when update has a valid command /pokedex but no parameter a default message should be sent"() {
@@ -135,9 +223,26 @@ class TelegramBotSpec extends Specification {
 
         and: "expect error since we are not sending the message when testing"
         thrown(NullPointerException)
-
-
     }
+
+    def "when update has a valid command /pokedex but no parameter it will be log to the db"() {
+
+        given: "an update request with a valid command but no parameter"
+        update.hasMessage() >> true
+        update.getMessage() >> message
+        message.hasText() >> true
+        message.getText() >> "/pokedex"
+
+        when: "onUpdateReceived is called"
+        telegramBot.onUpdateReceived(update)
+
+        then: "it will be log to the db"
+        1 * dao.saveBotUsage('/pokedex')
+
+        and: "expect error since we are not sending the message when testing"
+        thrown(NullPointerException)
+    }
+
 
     def "when update has a valid pokemon command information about pokemon should be sent"(){
 
@@ -147,12 +252,39 @@ class TelegramBotSpec extends Specification {
         message.hasText() >> true
         message.getText() >> "/pokemon pikachu"
 
-        when: "onUpdateReceived is called"
+        and: "the dao object returns the pokemon"
+        dao.getPokemonWithName('pikachu') >> new Pokemon(25, 'pikachu', 'electric', 1,
+                'Thunder Shock', 'Thunder', 'Quick Attack', 'Thunderbolt');
+
+        when: "onUpdateReceived is called and the dao object return the pokemon"
         telegramBot.onUpdateReceived(update)
 
         then: "information about the pokemon is sent"
-        1 * sendMessageRequest.setText('Number: 0\nPokemon: \nType: \nBuddy Distance: 0km\nBest Offence move set: ' +
-                '/\nBest Defensive move set: /')
+        1 * sendMessageRequest.setText('Number: 25\nPokemon: pikachu\nType: electric\nBuddy Distance: 1km\n' +
+                'Best Offence move set: Thunder Shock/Thunder\nBest Defensive move set: Quick Attack/Thunderbolt')
+
+        and: "expect error since we are not sending the message when testing"
+        thrown(NullPointerException)
+
+    }
+
+    def "when update has a valid pokemon command it should be log to the db"(){
+
+        given: "an update request with a valid message"
+        update.hasMessage() >> true
+        update.getMessage() >> message
+        message.hasText() >> true
+        message.getText() >> "/pokemon pikachu"
+
+        and: "the dao object returns the pokemon"
+        dao.getPokemonWithName('pikachu') >> new Pokemon(25, 'pikachu', 'electric', 1,
+                'Thunder Shock', 'Thunder', 'Quick Attack', 'Thunderbolt');
+
+        when: "onUpdateReceived is called and the dao object return the pokemon"
+        telegramBot.onUpdateReceived(update)
+
+        then: "information about the pokemon is sent"
+        1 * dao.saveBotUsage('/pokemon pikachu')
 
         and: "expect error since we are not sending the message when testing"
         thrown(NullPointerException)
@@ -167,18 +299,43 @@ class TelegramBotSpec extends Specification {
         message.hasText() >> true
         message.getText() >> "/type fire"
 
+        and: "the dao returns the type"
+        dao.getTypeWithName('fire') >> (new Type('fire',['Steel'], ['ground']))
+
         when: "onUpdateReceived is called"
         telegramBot.onUpdateReceived(update)
 
         then: "information about the pokemon is sent"
-        1 * sendMessageRequest.setText('Type: \n  Strong Against: \n  Weak Against: \n')
+        1 * sendMessageRequest.setText('Type: FIRE\n  Strong Against: \n    - Steel\n  Weak Against: \n    - ground\n')
 
         and: "expect error since we are not sending the message when testing"
         thrown(NullPointerException)
 
     }
 
-    def "when update has a valid pokedex number command pokemon should be sent"(){
+    def "when update has a valid type command it should be log to the db"(){
+
+        given: "an update request with a valid message"
+        update.hasMessage() >> true
+        update.getMessage() >> message
+        message.hasText() >> true
+        message.getText() >> "/type fire"
+
+        and: "the dao returns the type"
+        dao.getTypeWithName('fire') >> (new Type('fire',['Steel'], ['ground']))
+
+        when: "onUpdateReceived is called"
+        telegramBot.onUpdateReceived(update)
+
+        then: "it will be log to the db"
+        1 * dao.saveBotUsage('/type fire')
+
+        and: "expect error since we are not sending the message when testing"
+        thrown(NullPointerException)
+
+    }
+
+    def "when update has a valid pokedex command pokemon should be sent"(){
 
         given: "an update request with a valid message"
         update.hasMessage() >> true
@@ -186,12 +343,38 @@ class TelegramBotSpec extends Specification {
         message.hasText() >> true
         message.getText() >> "/pokedex 1"
 
+        and: "the dao returns the pokemon"
+        dao.getPokemonWithNumber(1) >> new Pokemon(25, 'pikachu', 'electric', 1,
+                'Thunder Shock', 'Thunder', 'Quick Attack', 'Thunderbolt');
+
         when: "onUpdateReceived is called"
         telegramBot.onUpdateReceived(update)
 
         then: "information about the pokemon is sent"
-        1 * sendMessageRequest.setText('Number: 0\nPokemon: \nType: \nBuddy Distance: 0km\nBest Offence move set: ' +
-                '/\nBest Defensive move set: /')
+        1 * sendMessageRequest.setText('Number: 25\nPokemon: pikachu\nType: electric\nBuddy Distance: 1km\n' +
+                'Best Offence move set: Thunder Shock/Thunder\nBest Defensive move set: Quick Attack/Thunderbolt')
+
+        and: "expect error since we are not sending the message when testing"
+        thrown(NullPointerException)
+    }
+
+    def "when update has a valid pokedex command it should be log to the db"(){
+
+        given: "an update request with a valid message"
+        update.hasMessage() >> true
+        update.getMessage() >> message
+        message.hasText() >> true
+        message.getText() >> "/pokedex 1"
+
+        and: "the dao returns the pokemon"
+        dao.getPokemonWithNumber(1) >> new Pokemon(25, 'pikachu', 'electric', 1,
+                'Thunder Shock', 'Thunder', 'Quick Attack', 'Thunderbolt');
+
+        when: "onUpdateReceived is called"
+        telegramBot.onUpdateReceived(update)
+
+        then: "it will be log to the db"
+        1 * dao.saveBotUsage('/pokedex 1')
 
         and: "expect error since we are not sending the message when testing"
         thrown(NullPointerException)
